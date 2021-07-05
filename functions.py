891fcc469a19c71fd80d013618ee89c8cfe9097e
StevePirateX <sharkbait__@hotmail.com>
2021-07-05 21:40:38 +1000

    Added more chores than available and regenerate list

--- functions.py
+++ functions.py
@@ -1,9 +1,22 @@
 import constants
 from csv import reader
 import random
+import os
 
 
 def import_csv_file(filename: str) -> list:
+    """
+    Imports a csv file with headers and returns each row as a list. The csv
+    file must contain the headers (chore, frequency, description)
+
+    :param filename: `String` of the path of where the csv file is located
+    :return: The list of the rows in the csv file.
+    """
+    if not os.path.exists(filename):
+        file = open(filename, 'w+')
+        file.write(f"{constants.CSV_HEADERS}\n")
+        file.write(f"{constants.CSV_DEFAULT_ROW}\n")
+
     file = open(filename, 'r')
     dt = reader(file)
     data = list(dt)
@@ -16,7 +29,16 @@ def import_csv_file(filename: str) -> list:
     return rows
 
 
-def get_task_list(chores: list):
+def get_task_list(chores: list, num_chores: int) -> list:
+    """
+    Randomly selects `num_chores` number of tasks based upon the input
+    list chores. The frequency of chores also changes the liklihood that
+    the chore is selected.
+
+    :param chores: List of chores (Chore name, Frequency)
+    :param num_chores: How many chores to be returned in the list
+    :return: List of chores
+    """
     chores.sort(key=lambda z: int(z[1]), reverse=True)
 
     chore_names = []
@@ -27,9 +49,21 @@ def get_task_list(chores: list):
         chore.append(weight)
         weights.append(weight)
 
-    generated_chores = [random.choices(chore_names, weights=weights).pop()]
-    while len(generated_chores) < constants.NUM_CHORES:
+    generated_chores = []
+    chore_copy = []
+    if len(chores) < num_chores:
+        print("More chores are requested than are in list. Chores will "
+              "be duplicated.")
+        chore_copy = chores.copy()
+    while len(generated_chores) < num_chores:
         choice = random.choices(chore_names, weights=weights).pop()
-        if choice not in generated_chores:
+        if choice not in generated_chores or len(generated_chores) >= len(
+                chores):
             generated_chores.append(choice)
+        for index, chore in enumerate(chores):
+            if choice == chore[0]:
+                chores.remove(chore)
+                break
+        if len(chores) == 0:
+            chores = chore_copy.copy()
     return generated_chores
--- run.py
+++ run.py
@@ -2,18 +2,35 @@ import constants as c
 import functions as f
 
 
+def get_random_chores(chore_list: list, number_of_chores: int) -> None:
+    """
+    Function to generate the chore list. If the chore file doesn't
+    exist, a default csv is created
+
+    :return: None
+    """
+    selected_chores = f.get_task_list(chore_list, number_of_chores)
+    print(f"Generated chores = ", end="")
+    for index, value in enumerate(selected_chores):
+        if index < len(selected_chores) - 1:
+            print(value, end=", ")
+        else:
+            print(value)
+
+
 if __name__ == '__main__':
+    print("Welcome to Recurring Chores v{}".format(c.VERSION))
     print("Running recurring chores...")
     chores = f.import_csv_file(c.CHORE_LIST_CSV_NAME)
     if c.PRINT_INPUT_CHORE_LIST:
         print("Chores: ", end="")
         for i, chore in enumerate(chores):
-            print(f"{i+1}: {chore[0]} ({chore[1]})", end="\t")
+            print(f"{i + 1}: {chore[0]} ({chore[1]})", end="\t")
         print()
-    chore_list = f.get_task_list(chores)
-    print(f"Generated chores = ", end="")
-    for i, chore in enumerate(chore_list):
-        if i < len(chore_list) - 1:
-            print(chore, end=", ")
-        else:
-            print(chore)
+    get_random_chores(chores, c.NUM_CHORES)
+    while True:
+        entry = input(
+            "Press ENTER to generate a new set of chores... (q: quit): ")
+        if entry != "":
+            break
+        get_random_chores(chores, c.NUM_CHORES)
